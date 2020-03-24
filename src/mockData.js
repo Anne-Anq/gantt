3c00b57284cd16de55437367dba859e853bba6f2
Anne Anquetin <anne.anquetin@gmail.com>
2020-03-24 17:24:35 -0600

    add custom detail content to mockData

--- src/mockData.js
+++ src/mockData.js
@@ -12,11 +12,17 @@ export const mockData = [
     style: {
       text: '#f123321',
       bg: '#102591'
-    }
+    },
+    detailContent: [
+      { label: 'Event', value: 'event1Name' },
+      { label: 'Description', value: 'this is a dumb example of description' },
+      { label: 'Type', value: 'an event type' },
+      { label: 'priority', value: 'high' }
+    ]
   },
   {
     id: 'event2Id',
-    title: 'event2Name finish 21:56',
+    title: 'event2Name',
     startTime: new Date(
       'Sat Mar 14 2020 19:56:24 GMT-0600 (Mountain Daylight Time)'
     ),
@@ -27,11 +33,17 @@ export const mockData = [
     style: {
       text: '#f983321',
       bg: '#ad77ff'
-    }
+    },
+    detailContent: [
+      { label: 'Event', value: 'event2Name' },
+      { label: 'Description', value: 'something something' },
+      { label: 'Type', value: 'another event type' },
+      { label: 'priority', value: 'Low' }
+    ]
   },
   {
     id: 'event3Id',
-    title: 'event3Name finish:20;56',
+    title: 'event3Name',
     startTime: new Date(
       'Sat Mar 14 2020 20:36:24 GMT-0600 (Mountain Daylight Time)'
     ),
@@ -42,6 +54,11 @@ export const mockData = [
     style: {
       text: '#f933321',
       bg: '##3568ff'
-    }
+    },
+    detailContent: [
+      { label: 'Event', value: 'event3Name' },
+      { label: 'Type', value: 'yes' },
+      { label: 'priority', value: 'meidum' }
+    ]
   }
 ]
